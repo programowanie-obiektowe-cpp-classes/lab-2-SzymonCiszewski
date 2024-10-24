d75b55080cf3876af0e241f31b6b72499896f905
Szymon Ciszewski <szymon526.ciszewski@wp.pl>
2024-10-24 19:34:41 +0200

    Zejsciowka2

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -3,6 +3,23 @@
 #include "Resource.hpp"
 
 class ResourceManager
-{
-    // Twoja implementacja tutaj
+{   
+public:
+    ResourceManager() { 
+        Resource resource;
+        wynik = resource.get();
+    } 
+    double get() { return wynik; }
+
+    ResourceManager(const ResourceManager& manager) : wynik{manager.wynik} {}
+
+    ResourceManager& operator=(const ResourceManager& other) { 
+        return *this;
+    }
+
+    ~ResourceManager() = default;
+
+
+private:
+    double wynik;
 };
